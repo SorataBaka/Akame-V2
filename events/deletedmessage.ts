f77b1a2f5ddf6f31718b5c91e8bf6a1e419691cc
Christian Harjuno <harjuno.tian@gmail.com>
2022-01-09 21:08:49 +0700

    Fixed bug where deleted images by security is picked up by snipe command

--- events/deletedmessage.ts
+++ events/deletedmessage.ts
@@ -5,11 +5,24 @@ module.exports = {
   name: "snipehandler",
   eventName: "messageDelete",
   async execute(message:Message, client:ClientExtensionInterface) {
+    if(!message.member) return
     const wordList = fs.readFileSync(__dirname + "/../assets/JSON/words.json")
     const wordListJSON:string[] = JSON.parse(wordList.toString())
     for(const words of wordListJSON){
       const regexExpression = new RegExp(words, "i")
       if(regexExpression.test(message.content)){
+        return
+      }
+    }
+    const memberInfo = message.member
+    //Check if member joined server within the last 1 week
+    const memberJoined = memberInfo?.joinedAt as Date
+    const memberJoinedDate = new Date(memberJoined)
+    const currentDate = new Date()
+    const timeDifference = currentDate.getTime() - memberJoinedDate.getTime()
+    const timeDifferenceInDays = timeDifference / (1000 * 3600 * 24)
+    if(timeDifferenceInDays < 15){
+      if(message.attachments.size > 0 || message.content.endsWith(".jpg" || ".png" || ".jpeg" || ".heic")){
         return
       }
     }
